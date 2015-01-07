25b39291587dee287f6aa99ced0f5f978dea313c
Waylon Brown <waylonjbrown@gmail.com>
2015-01-06 22:58:13 -0600

    Initial commit

---
+++ app/src/main/java/appuccino/simplyscanpro/MainActivity.java
@@ -0,0 +1,61 @@
+package appuccino.simplyscanpro;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView mainText = (TextView)findViewById(R.id.mainText);
+        Typeface type = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Light.ttf");
+        mainText.setTypeface(type);
+
+        SpannableString s = new SpannableString("SimplyScan");
+        s.setSpan(new TypefaceSpan(this, "Dancing-Script.ttf"), 0, s.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+// Update the action bar title with the TypefaceSpan instance
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(s);
+
+//        TextView actionBarTitle = getActionBarTextView("mTitleTextView")
+//        actionBarTitle.setTextColor(getResources().getColor(R.color.white));
+//        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Dancing-Script");
+//        actionBarTitle.setTypeface(tf);
+//        actionBarTitle.setTextSize(35);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
